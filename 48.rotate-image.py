ba6836f9472cce52a1f77d08ef7b8dc4dd2b1c03
Risang Baskoro <contact@risangbaskoro.com>
2025-02-18 17:26:28 +0700

    48. Rotate Image
    https://leetcode.com/problems/rotate-image

---
+++ 48.rotate-image.py
@@ -0,0 +1,18 @@
+# 48. Rotate Image
+# https://leetcode.com/problems/rotate-image
+
+from typing import List
+
+
+class Solution:
+    def rotate(self, matrix: List[List[int]]) -> None:
+        """
+        Do not return anything, modify matrix in-place instead.
+        """
+        # Reverse the matrix
+        matrix.reverse()
+
+        # Transpose the matrix
+        for i in range(len(matrix)):
+            for j in range(i, len(matrix)):
+                matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
