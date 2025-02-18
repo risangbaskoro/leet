050625003b8d5ccf3390e9c264229e26c2335dff
Risang Baskoro <contact@risangbaskoro.com>
2025-02-18 22:05:19 +0700

    3248. Snake in Matrix
    https://leetcode.com/problems/snake-in-matrix

--- 3248.snake-in-matrix.py
+++ 3248.snake-in-matrix.py
@@ -7,16 +7,17 @@ from typing import List
 class Solution:
     def finalPositionOfSnake(self, n: int, commands: List[str]) -> int:
         directions = {
-            "UP": [0, -1],
+            "UP": [0, -n],
             "RIGHT": [1, 0],
-            "DOWN": [0, 1],
+            "DOWN": [0, n],
             "LEFT": [-1, 0]
         }
 
-        pos = [0, 0]
+        pos = 0
 
         for command in commands:
-            direction = directions[command]
-            pos = [pos[0]+direction[0], pos[1]+direction[1]]
+            pos += sum(directions[command])
 
-        return pos[0] + (pos[1]*n)
+        return pos
+
+# See: ./notes/3248.snake-in-matrix.md
